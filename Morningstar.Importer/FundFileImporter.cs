9cff6f1ae6acaa5199be06c4526f62d9f8973a5a
Norm Clerman <norm.clerman@gmail.com>
2017-07-28 20:45:09 -0700

    read all records in the csv file in one operation.

--- Morningstar.Importer/FundFileImporter.cs
+++ Morningstar.Importer/FundFileImporter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.IO;
 using CsvHelper;
@@ -8,22 +9,36 @@ namespace Morningstar.Importer
 {
     public class FundFileImporter
     {
+        /// <summary>
+        /// Purpose: Import fund data from a csv file from Morningstar
+        /// </summary>
+        /// <param name="fileName">The name of the file.</param>
+        /// <returns>A List of class Holding</returns>
+        /// <remarks>
+        /// Programmer: N. S. Clerman
+        /// 
+        /// Revisions:
+        /// 1) N. S. Clerman, 28-Jul-2017: Read the file in with a single
+        ///    operation.
+        /// </remarks>
         public static List<Holding> Import (string fileName)
         {
             List<Holding> result = new List<Holding>();
             using (StreamReader reader = File.OpenText(fileName))
             {
-                // result = new char[reader.BaseStream.Length];
-                // await reader.ReadAsync(result, 0, (int)reader.BaseStream.Length);
                 var fund_csv = new CsvReader(reader);
                 fund_csv.Configuration.RegisterClassMap<HoldingMap>();
                 fund_csv.Configuration.HasHeaderRecord = true;
-                while (fund_csv.Read())
+                // Code to read the file record by record.
+                /*while (fund_csv.Read())
                 {
                     Holding record = fund_csv.GetRecord<Holding>();
                     result.Add(record);
-                }
-             return result;
+                }*/
+
+                // Code to read all the records. (This requires Linq)
+                result = fund_csv.GetRecords<Holding>().ToList();
+                return result;
             }
         }
     }
